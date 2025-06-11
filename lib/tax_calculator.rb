869b96e950cba865d269b82e65d9c8963fcc1dfa
Petar Simonovic <petar@simonovic.com>
2025-06-11 21:28:31 +0100

    Remove unused var

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -6,7 +6,11 @@ class TaxCalculator
 
   def calculate(income)
     total_tax = 0
+
+    # batch the income into the tax bands
     batched_income = batch_income(income)
+
+    # calculate the rate for each band of income
     batched_income.each_with_index do |value, index| 
      rate = @rates[index][:percentage]
      total_tax += calculate_at_rate(value, rate)
@@ -17,20 +21,17 @@ class TaxCalculator
   private 
 
   def calculate_at_rate(amount, rate)
-    puts(amount, rate)
     amount * rate/100
   end
 
   def batch_income(income)
     current_rate = 0
     batched_income = []
-    income_left_to_tax = income
       @rates.each_with_index do |rate, index|
       if index == @rates.length - 1
         batched_income.push(income - current_rate)
         return batched_income
       elsif income > rate[:income_threshold] 
-          income_left_to_tax -= (rate[:income_threshold] - current_rate)
           batched_income.push(rate[:income_threshold] - current_rate)
           current_rate = rate[:income_threshold]
       else
