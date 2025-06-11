e87101c57ac6b2d4f606125a554fb1cbfc857dd4
Petar Simonovic <petar@simonovic.com>
2025-06-11 21:23:29 +0100

    Tests passing

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -4,10 +4,14 @@ class TaxCalculator
     @rates = rates 
   end
 
-  def calculate(amount)
-    puts("RATE!")
-    rate = @rates[0]
-    calculate_at_rate(amount, rate[:percentage])
+  def calculate(income)
+    total_tax = 0
+    batched_income = batch_income(income)
+    batched_income.each_with_index do |value, index| 
+     rate = @rates[index][:percentage]
+     total_tax += calculate_at_rate(value, rate)
+    end
+    total_tax
   end
 
   private 
@@ -15,5 +19,25 @@ class TaxCalculator
   def calculate_at_rate(amount, rate)
     puts(amount, rate)
     amount * rate/100
+  end
+
+  def batch_income(income)
+    current_rate = 0
+    batched_income = []
+    income_left_to_tax = income
+      @rates.each_with_index do |rate, index|
+      puts(index, @rates.length)  
+      if index == @rates.length - 1
+        batched_income.push(income - current_rate)
+        return batched_income
+      elsif income > rate[:income_threshold] 
+          income_left_to_tax -= (rate[:income_threshold] - current_rate)
+          batched_income.push(rate[:income_threshold] - current_rate)
+          current_rate = rate[:income_threshold]
+      else
+        batched_income.push(income - current_rate)
+        return batched_income
+      end
+    end
   end
 end
--- spec/tax_calculator_spec.rb
+++ spec/tax_calculator_spec.rb
@@ -4,13 +4,29 @@ rates = [
   { income_threshold: 15600, percentage: 10.5 },
   { income_threshold: 53500, percentage: 17.5 },
   { income_threshold: 78100, percentage: 30 },
-  { income_threshold: 18000, percentage: 33 },
-  { income_threshold: 15600, percentage: 39 }
+  { income_threshold: 180000, percentage: 33 },
+  { income_threshold: Float::INFINITY, percentage: 39 }
 ]
 RSpec.describe TaxCalculator do
-  it 'calculates tax correctly for the lowest rate' do
+  it 'calculates tax correctly for $10,000' do
     tax_calculator = TaxCalculator.new(rates)
     expect(tax_calculator.calculate(10000)).to eq(1050.00)
+  end
+
+  it 'calculates tax correctly for $35,000' do
+    tax_calculator = TaxCalculator.new(rates)
+    expect(tax_calculator.calculate(35000)).to eq(5033.00)
+  end
+
+  it 'calculates tax correctly for $100,000' do
+    tax_calculator = TaxCalculator.new(rates)
+    expect(tax_calculator.calculate(100000)).to eq(22877.50)
+  end
+
+
+  it 'calculates tax correctly for $220,000' do
+    tax_calculator = TaxCalculator.new(rates)
+    expect(tax_calculator.calculate(220000)).to eq(64877.50)
   end
 
 end
