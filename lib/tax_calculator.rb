e241fe67eef78cf7a7e55d20785e1d3af3b07d14
Petar Simonovic <petar@simonovic.com>
2025-06-12 07:53:10 +0100

    final refactor

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,45 +1,37 @@
 class TaxCalculator
 
-  def initialize(rates)
-    @rates = rates 
+  def initialize(bands)
+    @bands = bands 
   end
 
   def calculate(income)
-    total_tax = 0
-
-    # batch the income into the tax bands
-    batched_income = batch_income(income)
-
-    # calculate the rate for each band of income
-    batched_income.each_with_index do |value, index| 
-     rate = @rates[index][:percentage]
-     total_tax += calculate_at_rate(value, rate)
-    end
-    total_tax
+    return 0 unless income
+    calculate_taxes_for_bands(income)
   end
 
   private 
 
   def calculate_at_rate(amount, rate)
-    amount * rate/100
+    amount * rate/100.0
   end
 
-  def batch_income(income)
+  def calculate_taxes_for_bands(income)
+    total_tax = 0
     previous_threshold = 0
-    batched_income = []
-      @rates.each_with_index do |rate, index|
-      current_threshold = rate[:income_threshold]
-      if batching_finished?(index, income, current_threshold)
-        batched_income.push(income - previous_threshold)
-        return batched_income
+    @bands.each_with_index do |band, index|
+      current_threshold = band[:income_threshold]
+      if finished_band_calculations?(index, income, current_threshold)
+        total_tax += calculate_at_rate(income - previous_threshold, band[:rate])
+        return total_tax
       else
-          batched_income.push(current_threshold - previous_threshold)
+        amount = current_threshold - previous_threshold
+          total_tax += calculate_at_rate(amount, band[:rate])
           previous_threshold = current_threshold
       end
     end
   end
 
-  def batching_finished?(index, income, income_threshold)
-      index == @rates.length - 1 || income < income_threshold
+  def finished_band_calculations?(index, income, income_threshold)
+      index == @bands.length - 1 || income < income_threshold
   end
 end
--- spec/tax_calculator_spec.rb
+++ spec/tax_calculator_spec.rb
@@ -1,31 +1,36 @@
 require 'tax_calculator'
 
-rates = [
-  { income_threshold: 15600, percentage: 10.5 },
-  { income_threshold: 53500, percentage: 17.5 },
-  { income_threshold: 78100, percentage: 30 },
-  { income_threshold: 180000, percentage: 33 },
-  { income_threshold: Float::INFINITY, percentage: 39 }
+bands = [
+  { income_threshold: 15600, rate: 10.5 },
+  { income_threshold: 53500, rate: 17.5 },
+  { income_threshold: 78100, rate: 30 },
+  { income_threshold: 180000, rate: 33 },
+  { income_threshold: Float::INFINITY, rate: 39 }
 ]
 RSpec.describe TaxCalculator do
+
+   it 'calculates tax correctly for $0' do
+    tax_calculator = TaxCalculator.new(bands)
+    expect(tax_calculator.calculate(0)).to eq(0)
+  end
   it 'calculates tax correctly for $10,000' do
-    tax_calculator = TaxCalculator.new(rates)
+    tax_calculator = TaxCalculator.new(bands)
     expect(tax_calculator.calculate(10000)).to eq(1050.00)
   end
 
   it 'calculates tax correctly for $35,000' do
-    tax_calculator = TaxCalculator.new(rates)
+    tax_calculator = TaxCalculator.new(bands)
     expect(tax_calculator.calculate(35000)).to eq(5033.00)
   end
 
   it 'calculates tax correctly for $100,000' do
-    tax_calculator = TaxCalculator.new(rates)
+    tax_calculator = TaxCalculator.new(bands)
     expect(tax_calculator.calculate(100000)).to eq(22877.50)
   end
 
 
-  it 'calculatesd tax correctly for $220,000' do
-    tax_calculator = TaxCalculator.new(rates)
+  it 'calculates tax correctly for $220,000' do
+    tax_calculator = TaxCalculator.new(bands)
     expect(tax_calculator.calculate(220000)).to eq(64877.50)
   end
 
